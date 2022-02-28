3836ee07c57216da15a3221dbc743de7e7b2a646
Lusine Am <lusine.a.amiraghyan@gmail.com>
2022-02-28 18:47:12 +0400

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,63 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
 import "./styles/App.css";
-import PostList from "./components/PostList";
-import PostForm from "./components/PostForm";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./UI/modal/MyModal";
-import MyButton from "./UI/button/MyButton";
-import PostService from "./API/PostService";
-import { usePosts } from "./hooks/usePosts.js";
-import Loader from "./UI/loader/Loader";
-import { useFetching } from "./hooks/useFetching";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filter, setFilter] = useState({ sort: "", query: "" });
-  const [modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
-  });
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  const removePost = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-
   return (
-    <div className="App">
-      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
-        Create Posts
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ margin: "15px 0" }} />
-      <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && <h2 style={{textAlign: "center"}}>{postError}</h2>}
-      {isPostsLoading ? (
-        <div
-          style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
-        >
-          <Loader />
-        </div>
-      ) : (
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchedPosts}
-          title={"Post List 1"}
-        />
-      )}
-    </div>
+    <BrowserRouter>
+      <Route path="/posts">
+        <Posts />
+      </Route>
+    </BrowserRouter>
   );
 }
 
